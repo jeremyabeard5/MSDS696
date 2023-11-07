6305011b9884c8fd4de0e14a6c921eca8c351dbf
jeremyabeard5 <jeremyab5@gmail.com>
2023-11-06 22:27:21 -0700

    made some good headway on api usage!

--- flight-price-api.py
+++ flight-price-api.py
@@ -139,7 +139,15 @@ print(response.json())
 #https://rapidapi.com/ntd119/api/booking-com13
 url = "https://booking-com13.p.rapidapi.com/flights/one-way"
 
-querystring = {"location_from":"Denver, USA, Colorado","location_to":"Cincinnati, USA, Ohio","departure_date":"2023-12-19","page":"1","country_flag":"us","number_of_stops":"NonstopFlights"}
+year = 2024
+month = 9
+day = 19
+datecode = str(year) + '-' + str(month) + '-' + str(day)
+departure_city = 'Denver, USA, Colorado'
+arrival_city = 'Cincinnati, USA, Ohio'
+querystring = {"location_from":departure_city,"location_to":arrival_city,"departure_date":datecode,"page":"1","country_flag":"us","number_of_stops":"NonstopFlights"}
+#querystring = {"location_from":"Denver, USA, Colorado","location_to":"Cincinnati, USA, Ohio","departure_date":datecode,"page":"1","country_flag":"us","number_of_stops":"NonstopFlights"}
+#querystring = {"location_from":"Denver, USA, Colorado","location_to":"Cincinnati, USA, Ohio","departure_date":"2023-12-19","page":"1","country_flag":"us","number_of_stops":"NonstopFlights"}
 
 headers = {
 	"X-RapidAPI-Key": "347c541513msh80c201a41e39ddep1dd960jsnf10b2afec703",
@@ -149,9 +157,10 @@ headers = {
 response = requests.get(url, headers=headers, params=querystring)
 
 #print(response.json())
+filen = 'flights_' + datecode + '.json'
 try:
     response_json = response.json()
-    with open('flights.json', 'w') as outfile:
+    with open(filen, 'w') as outfile:
         json.dump(response_json, outfile, indent=4)
     #print(json.dumps(response_json, indent=4))
 except JSONDecodeError:
@@ -180,8 +189,62 @@ except JSONDecodeError:
 #   - departure time
 #   - arrival time
 #   - number of connections
+
+# For each Booking.com13 output (shown example in flights.json), we want to keep:
+# __typename: "Trip" > bounds > segments > arrivedAt, departuredAt, duration, flightNumber, destination>cityCode,cityName, marketingCarrier>code,name, operatingCarrier>code,name, origin>cityCode,cityName
+# __typename: "Trip" > travelerPrices > price > price>currency>code > value
+#   
+#       
     
-    
+# I want to build a database/dataframe that has the features above for all flights from DEN-CVG from 2023-11-18 to 2024-09-19
+# I want to test the following flights:
+#    DEN-CVG
+#    CVG-DEN
+
+#    DEN-ORD
+#    ORD-DEN
+
+#    DEN-LAX
+#    LAX-DEN
+
+#    DEN-ATL
+#    ATL-DEN
+
+#    DEN-DFW
+#    DFW-DEN
+
+#    DEN-JFK
+#    JFK-DEN
+
+#    DEN-SFO
+#    SFO-DEN
+
+#    DEN-SEA
+#    SEA-DEN
+
+#    DEN-LAS
+#    LAS-DEN
+
+#    DEN-PHX
+#    PHX-DEN
+
+#    DEN-CLT
+#    CLT-DEN
+
+#    DEN-MCO
+#    MCO-DEN
+
+#    DEN-EWR
+#    EWR-DEN
+
+#    DEN-MSP
+#    MSP-DEN
+
+#    DEN-MIA
+#    MIA-DEN
+
+#    This should be 30 flight searches every day for 10 months~~300 days, so about 9000 flight searches. 
+#    Each will return about 10 flights, so 90000 flights.
     
     
     
